a03c812188502a9ce697bc96a35f37a216c3dded
rmhorton <rmhorton@attotron.com>
2010-12-06 15:58:44 -0800

    Copyright notice added

--- apvenn_2way.R
+++ apvenn_2way.R
@@ -1,5 +1,18 @@
 # example program to plot approximate proportional Venn diagram for two sets
 
+# Copyright Robert M. Horton, 2010
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details (see 
+#    http://www.gnu.org/licenses)
+
 # set operations from base package:
 # union(x, y), intersect(x, y), setdiff(x, y), setequal(x, y), is.element(el, set)
 
